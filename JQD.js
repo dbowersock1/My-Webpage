e331002f762aabf01c0919038114ca6900cd0328
Dylan <Dbowersock18@gmail.com>
2021-06-04 11:43:10 -0600

    commit

--- JQD.js
+++ JQD.js
@@ -13,3 +13,24 @@ $(".header-right").click(function(){
 		scrollTop: $('.background_second').offset().top
 	},1250)
 });
+
+$("#h-soft").click(function(){
+	$("h1").css("color","green");
+	$('html, body').animate({
+		scrollTop: $('#SWDIV').offset().top-=50
+	},1250)
+});
+
+$("#h-engg").click(function(){
+	$("h1").css("color","green");
+	$('html, body').animate({
+		scrollTop: $('#ENGDIV').offset().top-=50
+	},1250)
+});
+
+$("#h-life").click(function(){
+	$("h1").css("color","green");
+	$('html, body').animate({
+		scrollTop: $('#LIFEDIV').offset().top-=50
+	},1250)
+});
