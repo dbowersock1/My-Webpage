2340d8670c283456f4554f517f0dba8293c18575
Dylan <Dbowersock18@gmail.com>
2021-06-09 18:09:05 -0600

    commit

--- JQD.js
+++ JQD.js
@@ -1,36 +1,20 @@
 // code for the Javascript
 // Name is JQD
 
-
-
-$(document).ready(function(){
-	$("h1").css("color","blue");
-});
-
-$(".header-right").click(function(){
-	$("h1").css("color","red");
-	$('html, body').animate({
-		scrollTop: $('.background_second').offset().top
-	},1250)
-});
-
 $("#h-soft").click(function(){
-	$("h1").css("color","green");
 	$('html, body').animate({
-		scrollTop: $('#SWDIV').offset().top-=50
+		scrollTop: $('#SWDIV').offset().top-=700
 	},1250)
 });
 
 $("#h-engg").click(function(){
-	$("h1").css("color","green");
 	$('html, body').animate({
-		scrollTop: $('#ENGDIV').offset().top-=50
+		scrollTop: $('#ENGDIV').offset().top-=700
 	},1250)
 });
 
 $("#h-life").click(function(){
-	$("h1").css("color","green");
 	$('html, body').animate({
-		scrollTop: $('#LIFEDIV').offset().top-=50
+		scrollTop: $('#LIFEDIV').offset().top-=700
 	},1250);
 });
